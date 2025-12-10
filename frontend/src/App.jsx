1f924716d6e8a6261e091802a3a5bd49e1b5dccd
Henry03HGBP <henry.bautista.prado@icloud.com>
2025-12-10 14:28:37 -0600

    tratando de conectar backend con frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import io from 'socket.io-client';
+
+const SOCKET = io("http://localhost:4000/"); 
+
+function App() {
+  return (
+    <div>hola </div>
+  )
+}
+export default App
--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from 'express'
+import http from 'http'
+import {Server as SocketServer} from 'socket.io'
 
 const app = express();
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+    cors: {
+        origin: "http://localhost:5173/"
+    }
+})
 
-app.listen(3000)
-console.log('Servidor en en el puerto', 3000)
+io.on('connection', SOCKET => {
+    console.log('Cliente conectado')
+})
+
+server.listen(4000)
+console.log('Servidor en en el puerto', 4000)
